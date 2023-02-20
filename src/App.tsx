b63ce81b31190b65052f3345af643dc1ebabb0fe
Jessie <974889924@qq.com>
2023-02-20 17:43:37 +0800

    feat: alert message when user don't use mobile device

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useEffect} from 'react'
 import {HashRouter, Route, Routes} from "react-router-dom";
 import HomePage from "./pages/homePage";
 import SelectProvince from "./pages/selectProvince";
@@ -7,6 +7,12 @@ import ProvinceInfo from "./components/homePage/provinceInfo";
 import NotFound from "./pages/NotFound";
 
 const App = (): JSX.Element => {
+    useEffect(() => {
+        const isMobile = /mobile/gi.test(navigator.userAgent);
+        if (!isMobile) {
+            alert('欢迎 建议您用移动端访问该网站,以获得更好地浏览体验哦 ♪(^∇^*)');
+        }
+    },[])
     return (
         <HashRouter>
             <Routes>
